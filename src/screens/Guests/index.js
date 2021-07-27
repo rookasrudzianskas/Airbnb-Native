9de9846495160ff24337cc137adf15b34b6a4529
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 12:25:03 +0300

    Rokas

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Pressable, Text, View} from "react-native";
 import styles from "./styles";
 
 const GuestsScreen = () => {
+
+    const [adults, setAdults] = useState(0);
+
     return (
         <View>
             <View style={styles.row}>
@@ -13,14 +16,14 @@ const GuestsScreen = () => {
 
                 <View style={styles.buttons}>
 
-                    <Pressable onPress={() => console.log("- is clicked")} style={styles.button}>
+                    <Pressable onPress={() => setAdults(Math.max(0, adults - 1))} style={styles.button}>
                         <Text style={styles.minusAndPlus}>-</Text>
                     </Pressable>
 
-                    <Text style={styles.count}>0</Text>
+                    <Text style={styles.count}>{adults}</Text>
 
 
-                    <Pressable onPress={() => console.log("+ is clicked")} style={styles.button}>
+                    <Pressable onPress={() => setAdults(Math.max(1, adults + 1))} style={styles.button}>
                         <Text style={styles.minusAndPlus}>+</Text>
                     </Pressable>
 
