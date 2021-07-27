8182b4e4a74d375f1b6fb870c81f8cf3378c9776
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 11:08:22 +0300

    Rokas

--- App.js
+++ App.js
@@ -6,13 +6,14 @@ import Post from "./src/components/Post";
 import feed from "./assets/data/feed";
 
 const post1 = feed[0];
+const post2 = feed[1];
 
 export default function App() {
   return (
     <SafeAreaView>
       <StatusBar style="auto" />
         {/*<Home />*/}
-        <Post post={post1} />
+        <Post post={post2} />
     </SafeAreaView>
   );
 }
