5c9eb3338c0c07590087d6a04c26fc80b7f901a6
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 14:47:21 +0300

    Rokas

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,23 +1,26 @@
 import React, {useState} from 'react';
-import {Text, View, TextInput, FlatList} from "react-native";
+import {Text, View, TextInput, FlatList, Pressable} from "react-native";
 import styles from "./styles";
 import searchResults from "../../../assets/data/search";
 import {Entypo} from "@expo/vector-icons";
+import {useNavigation} from "@react-navigation/native";
 
 const DestinationSearchScreen = (props) => {
 
     const [inputText, setInputText] = useState("");
+    const navigation = useNavigation();
+
 
     return (
         <View style={styles.container}>
             <TextInput placeholder="Where are you Going? " value={inputText} onChangeText={(text) => setInputText(text)} style={styles.textInput} />
             <FlatList data={searchResults} renderItem={({item}) => (
-                <View style={styles.row}>
+                <Pressable style={styles.row} onPress={() => navigation.navigate("Guests")}>
                     <View style={styles.iconContainer}>
                         <Entypo name="location-pin" size={30} color={"#050505"} />
                     </View>
                     <Text style={styles.locationText}>{item.description}</Text>
-                </View>
+                </Pressable>
                 )}
               />
         </View>
