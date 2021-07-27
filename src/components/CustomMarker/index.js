c907256201a438266464bafdbe5cc7f9e5b1dc0f
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 18:03:22 +0300

    Rokas

--- src/components/CustomMarker/index.js
+++ src/components/CustomMarker/index.js
@@ -4,14 +4,14 @@ import {Text, View} from "react-native";
 
 const CustomMarker = (props) => {
 
-    const {coordinate} = props;
+    const {coordinate, price} = props;
 
     return (
          <Marker
-             coordinate={{latitude: 37.78825, longitude: -122.4324,}}>
+             coordinate={coordinate}>
 
              <View style={{backgroundColor: "white", padding: 5, borderRadius: 20, borderColor: "grey", borderWidth: 1,}}>
-                 <Text style={{fontWeight: "bold", fontSize: 10}}>$300</Text>
+                 <Text style={{fontWeight: "bold", fontSize: 10}}>${price}</Text>
              </View>
 
          </Marker>
