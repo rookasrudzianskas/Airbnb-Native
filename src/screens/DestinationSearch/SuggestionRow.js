d3ab67995a5528300e7dc7ff8730bb2149bf3104
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 17:03:19 +0300

    Rokas

--- src/screens/DestinationSearch/SuggestionRow.js
+++ src/screens/DestinationSearch/SuggestionRow.js
@@ -6,6 +6,7 @@ import {useNavigation} from "@react-navigation/native";
 
 
 
+
 const SuggestionRow = ({item}) => (
 
             <Pressable style={styles.row} onPress={() => console.log("ROOOOKAS")}>
