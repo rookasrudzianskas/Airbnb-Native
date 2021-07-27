a27e62d94ccdbc796a0d3e5dc1a003d33adba1eb
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 14:42:41 +0300

    Rokas

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View} from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
-import {Fontisto} from "@expo/vector-icons";
+import {EvilIcons, Feather, FontAwesome, FontAwesome5, Fontisto} from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,17 +13,32 @@ const HomeTabNavigator = () => {
                     <Fontisto name="search" size={25} color={color} />
                 )
             }} />
+
+            <Tab.Screen name={"Saved"} component={Home} options={{tabBarIcon: ({color}) => (
+                    <FontAwesome name="heart-o" size={25} color={color} />
+
+                )
+            }} />
+
+            <Tab.Screen name={"Trips"} component={Home} options={{tabBarIcon: ({color}) => (
+                    <FontAwesome5 name="airbnb" size={25} color={color} />
+                )
+            }} />
+
+            <Tab.Screen name={"Inbox"} component={Home} options={{tabBarIcon: ({color}) => (
+                    <Feather name="message-square" size={25} color={color} />
+                )
+            }} />
+
+            <Tab.Screen name={"Profile"} component={Home} options={{tabBarIcon: ({color}) => (
+                    <EvilIcons name="user" size={25} color={color} />
+                )
+            }} />
+
         </Tab.Navigator>
     );
 };
 
 export default HomeTabNavigator;
 
-// <FontAwesome name="heart-o" size={25} color={color} />
-//
-// <FontAwesome5 name="airbnb" size={25} color={color} />
-//
-// <Feather name="message-square" size={25} color={color} />
-//
-// <EvilIcons name="user" size={25} color={color} />
 
