dd0079dbd17d04246cbfd82908c45590f6afd459
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 13:59:28 +0300

    Rokas

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "../screens/Home";
 import DestinationSearchScreen from "../screens/DestinationSearch";
+import GuestsScreen from "../screens/Guests";
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,15 @@ const Stack = createStackNavigator();
 const Router = (props) => {
     return (
             <Stack.Navigator>
-                <Stack.Screen name="Destination Search" component={DestinationSearchScreen} />
+                <Stack.Screen name="Destination Search" component={DestinationSearchScreen} options={{
+                    title: "Search your destination"
+                }} />
+
+                <Stack.Screen name="Guests" component={GuestsScreen} options={{
+                    title: "How many people?"
+                }} />
+
+
                 {/*<Stack.Screen name="Home" component={Home} />*/}
 
             </Stack.Navigator>
