226a9e43b3c3c920cb101364d3ef0cad76c80f72
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 09:36:12 +0300

    Rokas

---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Something Cool in here</Text>
+        </View>
+    );
+};
+
+export default Home;
