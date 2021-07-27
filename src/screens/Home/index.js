a7d0866eb7afe6678d805d2d0ec3b3a887efb572
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 09:56:44 +0300

    Rokas

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {ImageBackground, Pressable, Text, View} from "react-native";
 import styles from "./styles";
+import { Fontisto } from '@expo/vector-icons';
 
 const Home = () => {
     return (
         <View>
+
+            <Pressable style={styles.searchButton} onPress={() => console.log("Search button clicked")}>
+                <Fontisto name="search" size={25} color={color} />;
+                <Text style={styles.buttonText}>Where are you going?</Text>
+            </Pressable>
+
             <ImageBackground source={require("../../../assets/images/wallpaper.jpg")} style={styles.image} >
                 <Text style={styles.title}>Go Near</Text>
 
