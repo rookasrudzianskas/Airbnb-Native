ce5c90cfc9f3d09ba108770e5d46bbd4bc5dbf49
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 14:46:01 +0300

    Rokas

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {ImageBackground, Pressable, SafeAreaView, Text, View} from "react-native";
 import styles from "./styles";
 import { Fontisto } from '@expo/vector-icons';
+import {useNavigation} from "@react-navigation/native";
 
 const Home = () => {
+
+    const navigation = useNavigation();
+
     return (
         <>
             <View>
-            <Pressable style={styles.searchButton} onPress={() => console.log("")}>
+            <Pressable style={styles.searchButton} onPress={() => navigation.navigate("Destination Search")}>
                 <Fontisto name="search" size={25} color={"#f15454"} />
                 <Text style={styles.searchButtonText}>Where are you going?</Text>
             </Pressable>
@@ -18,7 +22,7 @@ const Home = () => {
 
                 <Text style={styles.title}>Go Near</Text>
 
-                <Pressable style={styles.button} onPress={() => console.log("")}>
+                <Pressable style={styles.button} onPress={() => navigation.navigate("Destination Search")}>
                     <Text style={styles.buttonText}>Explore nearby stays</Text>
                 </Pressable>
             </ImageBackground>
