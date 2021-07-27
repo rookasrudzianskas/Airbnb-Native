ff9fbf523263e28d0ffae772c42168679734f0f2
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 14:29:58 +0300

    Rokas

---
+++ src/navigation/HomeTabNavigator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from "react-native";
+
+const HomeTabNavigator = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default HomeTabNavigator;
