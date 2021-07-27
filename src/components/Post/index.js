ac1a293b60f6ff46809c0deb8210ce785d5287b0
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 10:21:06 +0300

    Rokas

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View} from "react-native";
+import {Image, View} from "react-native";
 import styles from "./styles";
 
 const Post = (props) => {
     return (
         <View style={styles.container}>
-
+            <Image style={styles.image} source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'}} />
         </View>
     );
 };
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,7 +1,15 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-
+    container: {
+        margin: 10,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 3/2,
+        resizeMode: "cover",
+        borderRadius: 20,
+    }
 
 });
 
