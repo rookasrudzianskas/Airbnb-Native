6476ee63fda0cddc600072d6d72627c34be60e27
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 12:05:36 +0300

    Rokas

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import styles from "./styles";
 
 const GuestsScreen = () => {
     return (
         <View>
+            <View style={styles.row}>
 
+            </View>
         </View>
     );
 };
