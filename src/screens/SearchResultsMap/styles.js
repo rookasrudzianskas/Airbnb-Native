6cd37291cac6f3264a7cbe98d016d5d9f8bdd4e9
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 17:15:31 +0300

    Rokas

--- src/screens/SearchResultsMap/styles.js
+++ src/screens/SearchResultsMap/styles.js
@@ -2,14 +2,16 @@ import {Dimensions, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        // flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        width: '100%',
+        height: '100%',
     },
     map: {
         width: Dimensions.get('window').width,
-        height: 2000,
+        height: Dimensions.get('window').height,
     },
 });
 
