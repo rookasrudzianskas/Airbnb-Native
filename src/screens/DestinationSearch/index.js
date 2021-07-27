1ecfe768f6e750802d0f58badaacc1246bda90a7
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 11:32:41 +0300

    Rokas

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Text, View, TextInput} from "react-native";
+import styles from "./styles";
 
 const DestinationSearchScreen = () => {
     return (
         <View>
+            <TextInput placeholder="Where are you Going? " style={styles.textInput} />
 
         </View>
     );
