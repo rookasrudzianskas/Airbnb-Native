bca12a7f48abbadc06e678462a44c1b248373fbc
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 12:28:03 +0300

    Rokas

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -5,6 +5,8 @@ import styles from "./styles";
 const GuestsScreen = () => {
 
     const [adults, setAdults] = useState(0);
+    const [children, setChildren] = useState(0);
+    const [infants, setInfants] = useState(0);
 
     return (
         <View>
@@ -29,6 +31,52 @@ const GuestsScreen = () => {
 
                 </View>
             </View>
+
+            <View style={styles.row}>
+                <View>
+                    <Text style={styles.adults}>Children</Text>
+                    <Text style={styles.agesAdults}>Ages 2-12</Text>
+                </View>
+
+                <View style={styles.buttons}>
+
+                    <Pressable onPress={() => setChildren(Math.max(0, children - 1))} style={styles.button}>
+                        <Text style={styles.minusAndPlus}>-</Text>
+                    </Pressable>
+
+                    <Text style={styles.count}>{children}</Text>
+
+
+                    <Pressable onPress={() => setChildren(Math.max(1, children + 1))} style={styles.button}>
+                        <Text style={styles.minusAndPlus}>+</Text>
+                    </Pressable>
+
+                </View>
+            </View>
+
+            <View style={styles.row}>
+                <View>
+                    <Text style={styles.adults}>Infants</Text>
+                    <Text style={styles.agesAdults}>Under 2</Text>
+                </View>
+
+                <View style={styles.buttons}>
+
+                    <Pressable onPress={() => setInfants(Math.max(0, infants - 1))} style={styles.button}>
+                        <Text style={styles.minusAndPlus}>-</Text>
+                    </Pressable>
+
+                    <Text style={styles.count}>{infants}</Text>
+
+
+                    <Pressable onPress={() => setInfants(Math.max(1, infants + 1))} style={styles.button}>
+                        <Text style={styles.minusAndPlus}>+</Text>
+                    </Pressable>
+
+                </View>
+            </View>
+
+
         </View>
     );
 };
