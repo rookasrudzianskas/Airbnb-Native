2bd1b1808d26062a24d049e6e554b3607a3af4d0
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 14:32:17 +0300

    Roaks

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {View} from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 const HomeTabNavigator = () => {
     return (
