cf670e05e7a69c08f28385064b92c9548f6950c4
Rokas <rookas.rudzenskas@gmail.com>
2021-07-27 12:12:28 +0300

    Rokas

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Pressable, Text, View} from "react-native";
 import styles from "./styles";
 
 const GuestsScreen = () => {
     return (
         <View>
             <View style={styles.row}>
+                <View>
+                    <Text style={styles.adults}>Adults</Text>
+                    <Text style={styles.agesAdults}>Ages 13 or above</Text>
+                </View>
 
+                <View>
+
+                    <Pressable onPress={() => console.log("- is clicked")} style={styles.button}>
+                        <Text>-</Text>
+                    </Pressable>
+
+                    <Text>0</Text>
+
+
+                    <Pressable onPress={() => console.log("+ is clicked")} style={styles.button}>
+                        <Text>+</Text>
+                    </Pressable>
+
+                </View>
             </View>
         </View>
     );
--- src/screens/Guests/styles.js
+++ src/screens/Guests/styles.js
@@ -1,6 +1,22 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 20,
+        borderBottomWidth: 1,
+        borderColor: "lightgrey",
+    },
+    adults: {
+        fontWeight: "bold",
+        fontSize: 22,
+
+    },
+    agesAdults: {
+        color: "#8d8d8d",
+        fontSize: 17,
+    },
 
 });
 
