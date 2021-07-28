4ac613ca59c5c1fed24102f80498906c279fc37c
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 11:34:31 +0300

    Rokas

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Pressable, SafeAreaView, Text, View} from "react-native";
+import { Auth } from 'aws-amplify';
+
+
+async function signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+}
 
 const Profile = () => {
     return (
         <SafeAreaView>
             <View style={{height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <Pressable style={{width: '100%', height: 40, backgroundColor: "#ea5151", alignItems: "center", justifyContent: "center"}}>
-                    <Text style={{alignItems: "center"}}>Log Out</Text>
+                <Pressable onPress={signOut} style={{width: '100%', height: 40, backgroundColor: "#ea5151", alignItems: "center", justifyContent: "center"}}>
+                    <Text style={{alignItems: "center", fontWeight: "bold", color: "white"}}>Log Out</Text>
                 </Pressable>
             </View>
         </SafeAreaView>
