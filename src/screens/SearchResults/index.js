670c907d4d937ff447f3fba493c0b677d1172d1a
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 12:36:06 +0300

    Rokas

--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -2,13 +2,23 @@ import React, {useEffect} from 'react';
 import {Text, View, FlatList} from "react-native";
 import feed from "../../../assets/data/feed";
 import Post from "../../components/Post";
+import {API, graphqlOperation} from "aws-amplify";
+import {listPosts} from "../../graphql/queries";
 
 const SearchResults = () => {
 
     useEffect(() => {
         const fetchPosts = async () => {
-
+            try {
+                const postsResult = await API.graphql(graphqlOperation(listPosts));
+                console.log(postsResult);
+            } catch (e) {
+                console.log(e);
+            }
         };
+
+        fetchPosts();
+
     }, []);
 
     return (
